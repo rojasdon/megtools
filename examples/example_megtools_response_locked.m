75d88726316858f18ba30dfcf4c426d0328d7715
Unknown <don.rojas@colostate.edu>
2017-07-24 13:15:32 -0600

    example edits

--- examples/example_megtools_response_locked.m
+++ examples/example_megtools_response_locked.m
@@ -1,111 +1,59 @@
 % example script using various megtools programs to demonstrate various
 % capabilities
 
-% set defaults
-meg_defaults;
-
-% directory for data
-pth = fullfiles(which('ft_defaults'),'sample_data');
-cd(pth);
-
-% import a continuous file - by default the sample file in installation.
 % This is a left finger movement study - see paper for methods:
 %   Wilson, T. W. et al. (2010). An extended motor network generates beta 
 %       and gamma oscillatory perturbations during development. 
 %       Brain and cognition, 73(2), 75-84.
+
+% set defaults
+meg_defaults;
+
+% directory for data
+[pth,~,~] = fileparts(which('meg_defaults'));
+
+% check if data are downloaded
+if exist(fullfile(pth,'sample_data','motor'),'dir')
+    cd(fullfile(pth,'sample_data','motor'));
+else
+    error('Sample dataset not present!');
+end
+
+% import a continuous file - by default the sample file in installation.
 cnt = get4D('c,rfhp0.1Hz');
 
-% delete bad channels - input is single channel or vector e.g., [117 201]
-del = deleter(cnt,91);
+% there are no events in this file. We will create events from an
+% accelerometer auxillary channel. The accelerometer was attached to the
+% left index finger
 
-% separate continuous file into trials, or epochs
-type    = 'trigger'; % alternatively, can be 'response'
-start   = 200;  % prestim ms
-stop    = 1000; % poststim ms
-thresh  = 3000; % optional rejection threshold
-adj     = 20;   % optional adjustment of trigger latency
-epoched = epocher(del,type,start,stop,0);
+% get indices of external channels
+ext = meg_channel_indices(cnt,'multi','EXT');
 
-% uncomment next line to epoch with a threshold and and latency adjustment
-% epoched = epocher(del,'trigger',trigval,start,stop,adj,thresh);
+% create triggers from accelerometers
+vals                = [10 20]; % trigger codes, one for each accelerometer channel
+dur                 = 3000;    % duration re-triggering is prevented
+threshold           = 2.5;     % in sd units for trigger definition
+[accel, fchn, thresh] = process_auxiliary(cnt.data(ext(1:2),:),vals,cnt.sr,3000,threshold);
+newtrigs            = bitor(double(accel(1,:)),double(accel(2,:))); % combine into a single trigger channel
+trig                = meg_channel_indices(cnt,'multi','TRIGGER'); % trigger channel index
+cnt.data(trig,:)    = cnt.data(trig,:) + newtrigs; % adds values to existing triggers - if already empty, can just replace
+cnt.events          = create_events(cnt.data(trig,:),{'TRIGGER'});
 
-% if you have multiple trigger types and you want to concatenate them all
-% into a single epoch file, you could use the following code, assuming that
-% you'd separately epoched a set called epoched1 and another called
-% epoched2 (you can have as many MEG structs concatenated as you wish in a
-% single call to the function
+% delete bad channels
+del = cnt;
 
-% concatenated = concatMEG(epoched1,epoched2);
+% create trials from events
+start = 2500;
+stop = 2500;
+epoched = epocher(del,'trigger',start,stop);
 
-% you can save this file as an SPM8 formatted result as follows
-% D = meg2spm(concatenated);
+% extract only left finger epochs
+left =  epoch_extractor(epoched,10);
 
-% you could also save this file as an EEGLAB formatted file as follows
-% EEG = meg2eeglab(concatenated);
+% DC offset correction epochs using custom window
+left = offset(left,[-2500 -2000]);
 
-% DC offset correction epochs - default is prestim window if present
-epoched = offset(epoched);
-
-% average epochs (note you could also reject on amplitude in epocher
-% you should offset before averaging with a rejection threshold
-trig = 40;
-avg  = averager(epoched,thresh,trig);
-
-% filter avg with a few different filters - only works if signal processing
-% toolbox installed
-order = 4; low = 20; high = 55;
-lp   = filterer(avg,order,'low',low);
-hp = filterer(avg,order,'high',2);
-bp   = filterer(avg,order,'band',low,high);
-
-% offset correction
-corr = offset(lp);
-hp   = offset(hp);
-bp   = offset(bp);
-
-% you could combine some or all of these calls in one line, as follows:
-% corr=offset(filterer(averager(epoched,thresh),4,'low',30));
-
-% get root mean square of waveform
-rms  = xrms(corr,1);
-
-% butterfly plot scaled to fT
-figure('name','Averaged Evoked Field','color',[.8 .8 .8]);
-subplot(2,1,1);plot(corr.time,corr.data*1E15); 
-title('Averaged Evoked Field');
-axis tight;xlabel('Time (ms)');ylabel('Amplitude fT');
-subplot(2,1,2);plot(rms.time,rms.data);
-title('Root Mean Squared Amplitude');
-axis tight;xlabel('Time (ms)');ylabel('Amplitude fT');
-
-% compare various filtered datasets
-figure('name','Various Filters','color',[.8 .8 .8]);
-subplot(3,1,1);plot(corr.time,corr.data*1E15); 
-title('20 Hz low pass');axis tight;xlabel('Time (ms)');ylabel('Amplitude fT');
-subplot(3,1,2);plot(hp.time,hp.data);
-title('2 Hz high pass');axis tight;xlabel('Time (ms)');ylabel('Amplitude fT');
-subplot(3,1,3);plot(bp.time,bp.data);
-title('20-55 Hz band pass');axis tight;xlabel('Time (ms)');ylabel('Amplitude fT');
-
-% topo plot of peak at 155 msec
-figure('name','flat topography','color',[.8 .8 .8]);
-meg_plot2d(corr,155);
-
-% 3d topo plot at same point
-figure('name','3D topography','color',[.8 .8 .8]);
-subplot(1,2,1); meg_plot3d(corr,155); title('3D topography');
-
-% uncomment to show headshape too just for fun
-% hold on;
-% scalp = triangulate_meg(corr.fiducials.pnt);
-% s = patch('faces',scalp,'vertices',corr.fiducials.pnt,'Edgecolor','r',...
-%       'Facelighting','none','Facecolor','none','Marker','o',...
-%        'Linestyle','none');
-
-% demonstrate simple plot function for just headshape, fids and locations
-subplot(1,2,2); plot_hs_sens(corr); title('3D headshape, fids and locations');
-
-% time-frequency plot from channel 181(180) in the helmet from 5 to 80 Hz
-tf = tft(epoched,[5 80],181,'waven',7);
-figure('name','Morlet Wavelet Analysis in Gamma Band','color',[.8 .8 .8]);
-meg_plotTFR(tf,'nepower');
+% time-frequency plot from channel A82 in the helmet from 5 to 80 Hz
+tf = tft(left,[5 50],'chan',{'A146'},'waven',[4 12],'demean','no','detrend','yes');
+figure('name','Morlet Wavelet Analysis in Gamma Band','color','w');
+meg_plotTFR(tf,'nipower');
